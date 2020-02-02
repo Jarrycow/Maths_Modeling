5040899c3112f8e0c313feea6bce6560b6460eed
Jarrycow <kjr000614@163.com>
2020-02-03 00:12:56 +0800

    Update min_Routie.m

--- 图/min_Routie.m
+++ 图/min_Routie.m
@@ -1,8 +1,9 @@
-function D=min_Routie(G)
+function [P,D]=min_Routie(G,i,j)
 % % s为顶点1 t为顶点2 w为顶点3
-% % function D=min_Routie(s,t,w)
-% % G = graph(s,t,w);
-
-
+% % function [P,D]=min_Routie(s,t,w,i,j)
+% % G = graph(s,t,w); %生成了图
+[P,D] = shortestpath(G,i,j);
+myplot = plot(G, 'EdgeLabel', G.Edges.Weight, 'linewidth', 2);  %首先将图赋给一个变量
+highlight(myplot, P, 'EdgeColor', 'r')
 
 end
