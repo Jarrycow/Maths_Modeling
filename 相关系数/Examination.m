c68d34b9fd1f13760524ac7f6d90d59f5a753650
Jarrycow <kjr000614@163.com>
2020-02-04 12:17:55 +0800

    数据分析

---
+++ 相关系数/Examination.m
@@ -0,0 +1,11 @@
+function RESULT=Examination(Test)
+MIN = min(Test);  % 每一列的最小值
+MAX = max(Test);   % 每一列的最大值
+MEAN = mean(Test);  % 每一列的均值
+MEDIAN = median(Test);  %每一列的中位数
+SKEWNESS = skewness(Test); %每一列的偏度
+KURTOSIS = kurtosis(Test);  %每一列的峰度
+STD = std(Test);  % 每一列的标准差
+RESULT = [MIN;MAX;MEAN;MEDIAN;SKEWNESS;KURTOSIS;STD];  %将这些统计量放到一个矩阵中中表示
+end
+
